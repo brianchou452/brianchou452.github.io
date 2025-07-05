54e6675e512e0e780baeab34ec4d432675e1e0fc
海獺 <brianchou452@gmail.com>
2025-07-05 22:58:57 +0800

    feat: 完成中文HeroBanner元件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,39 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import swup from "@swup/astro";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
 import remarkMath from "remark-math";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-import svelte from "@astrojs/svelte";
-
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://about.seaotter.cc",
-  integrations: [mdx(), sitemap(), svelte(), icon(), pagefind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    svelte(),
+    icon(),
+    pagefind(),
+    swup({
+      theme: false,
+      containers: ["main", "footer", ".banner-inner"],
+      smoothScrolling: true,
+      progress: true,
+      cache: true,
+      preload: true,
+      updateHead: true,
+      updateBodyClass: false,
+      globalInstance: true,
+    }),
+  ],
   i18n: {
     locales: ["en", "zh-tw", "zh-cn", "zh-hk", "zh-hant", "zh-sg", "zh"],
     defaultLocale: "zh-tw",
